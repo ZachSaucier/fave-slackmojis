0962cb57fb1ad1a236939f0d614866781601bfb3
ZachSaucier <zachsaucier@gmail.com>
2024-04-04 18:51:23 -0400

    Improved readme formatting hopefully

--- generate-readme.sh
+++ generate-readme.sh
@@ -4,9 +4,11 @@ rm README.md
 echo '# fave-slackmojis' >> README.md
 echo 'My favorite emojis to use in a Slack setting, saved here to make sure I can access them in future Slacks.' >> README.md
 
+echo 'This README was generated using `./generate-readme.sh`' >> README.md
+
+echo '| Emoji preview | Emoji name |' >> README.md
+echo '| ------------- | ------------- |' >> README.md
 for filename in ./emojis/**; do
   name=${filename##*\/}
-  echo "- <img src='$filename'/> - $name" >> README.md
+  echo "| <img src='$filename'/> | $name |" >> README.md
 done
-
-# find emojis -type f -exec echo "- <img src='./{}'> " \; >> README.md
