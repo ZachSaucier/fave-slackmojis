f6de0cb4e7d296dac50d79394298516fc09ed88e
ZachSaucier <zachsaucier@gmail.com>
2024-06-06 22:22:21 -0400

    Improved script

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # fave-slackmojis
-My favorite emojis to use in a Slack setting, saved here to make sure I can access them in future Slacks.
 This README was generated using `./generate-readme.sh`
 | Emoji preview | Emoji name |
 | ------------- | ------------- |
--- generate-readme.sh
+++ generate-readme.sh
@@ -2,7 +2,7 @@
 rm README.md
 
 echo '# fave-slackmojis' >> README.md
-echo 'My favorite emojis to use in a Slack setting, saved here to make sure I can access them in future Slacks.' >> README.md
+echo 'My favorite emojis to use in a Slack setting, saved here to make sure I can access them in future Slacks.' >> README.md\n
 
 echo 'This README was generated using `./generate-readme.sh`' >> README.md
 
@@ -10,6 +10,6 @@ echo '| Emoji preview | Emoji name |' >> README.md
 echo '| ------------- | ------------- |' >> README.md
 for filename in ./emojis/**; do
   name=${filename##*\/}
-  rm ${filename}:Zone.Identifier
+  rm -f ${filename}:Zone.Identifier
   echo "| <img src='$filename'/> | $name |" >> README.md
 done
